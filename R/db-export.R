b15fa7589b1e3a7b7787310db238b8907b6000d2
mrustl <michael.rustler@kompetenz-wasser.de>
2019-02-18 22:33:38 +0100

    convert R script to function (#3)

--- R/db-export.R
+++ R/db-export.R
@@ -1,9 +1,25 @@
-### R script for exporting tables and queries from MS ACCESS DB into 
-### .csv files (one for each table/query) into subfolder 
-### "qmra-db_accdb"
-devtools::install_github("kwb-r/kwb.db")
+#' Default Path to MS ACCESS Database 
+#' @return path to MS ACCESS DB
+#' @examples
+#' default_access_db()
+#' @export
+default_access_db <- function() {
+  
+system.file("qmra-db.accdb", package = "qmra.db")  
+  
+}
 
+#'  Dump MS ACCESS Database To CSV
+#'  
+#' @description for exporting tables and queries from MS ACCESS DB into 
+#' .csv files (one for each table/query) into subfolder "qmra-db_accdb"
+#' @param db_path path to MS Access DB (default: deufault_access_db())
+#' @importFrom kwb.utils safePath 
+#' @importFrom kwb.db dumpDatabase
+#' @export
+dump_access_db <- function(db_path = default_access_db()) {
 
-mdb_path <- kwb.utils::safePath("qmra-db.accdb")
+mdb_path <- kwb.utils::safePath(db_path)
 
-kwb.db::hsDumpMdb(mdb = mdb_path, ptrn = c("^tbl|^qry"))
+kwb.db::dumpDatabase(mdb = mdb_path, ptrn = c("^tbl|^qry"))
+}
